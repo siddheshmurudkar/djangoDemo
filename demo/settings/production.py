643a2ab663b80caf21b1dacc1dd72839a8becdd2
siddheshmurudkar <siddhesh@curiositygym.com>
2019-08-06 16:27:52 +0530

    added dashboard

---
+++ demo/settings/production.py
@@ -0,0 +1,10 @@
+# settings/production.py
+from .base import 
+import dj_database_url
+
+ENVIRONMENT = 'production'
+DEBUG = False
+ALLOWED_HOSTS = ['']
+DATABASES['default'] = dj_database_url.config(
+    default='DATABASE_URL_HERE'
+)
